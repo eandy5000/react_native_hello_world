e622c272a856ef29fc47f2ec3643438b9f1b19ae
Andy Eccleston <eandy5000@yahoo.com>
2017-01-30 20:20:15 -0600

    hello world on screen

---
+++ index.ios.js
@@ -0,0 +1,11 @@
+//iOS build root
+import React from 'react';
+import { Text, AppRegistry } from 'react-native';
+
+const App = () => {
+  return (
+    <Text>Hello World</Text>
+  );
+};
+
+AppRegistry.registerComponent('albums', () => App );
