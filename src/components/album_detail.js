5cdb2eeebfa45f8327ac646d9b9644f07ba4e652
Andy Eccleston <eandy5000@yahoo.com>
2017-01-31 15:36:34 -0600

    container are styled

--- src/components/album_detail.js
+++ src/components/album_detail.js
@@ -1,15 +1,55 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, View, Image } from 'react-native';
 import Card from './card';
+import CardSection from './card_section';
 
 const AlbumDetail = (props) => {
-    const { title } = props.album;
+    const { title, artist, thumbnail_image, image } = props.album;
+
+    const styles = {
+        headerContentStyle: {
+            flexDirection: 'column',
+            justifyContent: 'space-around'
+        },
+        headerText: {
+            fontSize: 18
+        },
+        thumbnailStyle: {
+            height: 50,
+            width: 50
+        },
+        thumbnailContainer: {
+            justifyContent: 'center',
+            alignItems: 'center',
+            marginLeft: 10,
+            marginRight: 10
+        },
+        image: {
+            height: 300,
+            width: null,
+            flex: 1
+        }
+    };
 
     return (
         <Card>
-            <Text>{ title }</Text>
+            <CardSection>
+              <View style={styles.thumbnailContainer} >
+                <Image style={styles.thumbnailStyle} source={{ uri: thumbnail_image }} />
+              </View>
+              <View style={styles.headerContentStyle} >
+                <Text style={styles.headerText} >{ title }</Text>
+                <Text>{ artist }</Text>
+              </View>
+            </CardSection>
+            <CardSection>
+                <Image 
+                    style={styles.image}
+                    source={{ uri: image }}
+                />
+            </CardSection>
         </Card>
     );
-};
+ };
 
 export default AlbumDetail;
