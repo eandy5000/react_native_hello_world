cba3226a231b7d476ec43f1e2bc38125dff113e0
Andy Eccleston <eandy5000@yahoo.com>
2017-02-01 15:35:06 -0600

    app basics are working

--- src/components/album_detail.js
+++ src/components/album_detail.js
@@ -1,12 +1,39 @@
 import React from 'react';
-import { Text, View, Image } from 'react-native';
+import { Text, View, Image, Linking } from 'react-native';
 import Card from './card';
 import CardSection from './card_section';
+import Button from './button';
 
 const AlbumDetail = (props) => {
-    const { title, artist, thumbnail_image, image } = props.album;
+    const { title, artist, thumbnail_image, image, url } = props.album;
 
-    const styles = {
+    return (
+        <Card>
+            <CardSection>
+              <View style={styles.thumbnailContainer} >
+                <Image style={styles.thumbnailStyle} source={{ uri: thumbnail_image }} />
+              </View>
+              <View style={styles.headerContentStyle} >
+                <Text style={styles.headerText} >{ title }</Text>
+                <Text>{ artist }</Text>
+              </View>
+            </CardSection>
+            <CardSection>
+                <Image 
+                    style={styles.image}
+                    source={{ uri: image }}
+                />
+            </CardSection>
+            <CardSection>
+                <Button onPress={() => Linking.openURL(url)}>
+                    Buy Now!
+                </Button>
+            </CardSection>
+        </Card>
+    );
+ };
+
+     const styles = {
         headerContentStyle: {
             flexDirection: 'column',
             justifyContent: 'space-around'
@@ -30,26 +57,5 @@ const AlbumDetail = (props) => {
             flex: 1
         }
     };
-
-    return (
-        <Card>
-            <CardSection>
-              <View style={styles.thumbnailContainer} >
-                <Image style={styles.thumbnailStyle} source={{ uri: thumbnail_image }} />
-              </View>
-              <View style={styles.headerContentStyle} >
-                <Text style={styles.headerText} >{ title }</Text>
-                <Text>{ artist }</Text>
-              </View>
-            </CardSection>
-            <CardSection>
-                <Image 
-                    style={styles.image}
-                    source={{ uri: image }}
-                />
-            </CardSection>
-        </Card>
-    );
- };
 
 export default AlbumDetail;
